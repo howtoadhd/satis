2ca1c2e153938f479342ba2e09ad378916baf17f
LoreleiAurora <iam@l5i.me>
2018-01-14 20:05:29 +0000

    Use gulp to build repo

---
+++ gulpfile.babel.js
@@ -0,0 +1,42 @@
+import gulp from 'gulp';
+import composer from 'gulp-composer';
+import gutil from 'gulp-util';
+import {spawn} from 'child_process';
+
+const
+    build_dir = 'public',
+    satis = {
+        binary: './vendor/bin/satis',
+        config: './satis.json',
+    };
+
+gulp.task("composer", () => {
+    composer({
+        async: false
+    });
+});
+
+gulp.task("satis", (cb) => {
+    const cmd = spawn(satis.binary, ['build', satis.config, build_dir, '--no-interaction']);
+    cmd.stdout.on('data', data => {
+        gutil.log(gutil.colors.green(data.toString()));
+    });
+
+    cmd.stderr.on('data', data => {
+        gutil.log(gutil.colors.red(data.toString()));
+    });
+
+    cmd.on('exit', code => {
+        if (code > 0) {
+            cb('Satis exited with code ' + code.toString());
+        } else {
+            cb();
+        }
+    });
+});
+
+gulp.task('prepare', ['composer']);
+
+gulp.task('build', ['satis']);
+
+gulp.task('default', ['prepare', 'build']);
